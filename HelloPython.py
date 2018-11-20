7c1717c09d4fe407707eb31a742e87186929e000
Jonathan Creedon <jonathan.creedon@mycit.ie>
2018-11-20 11:38:42 +0000

    Made change to repository

--- HelloPython.py
+++ HelloPython.py
@@ -1,6 +1,7 @@
 import datetime
 print("Hello world!")
 print("file change")
+print("Git Bash change")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
